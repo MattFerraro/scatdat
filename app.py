319992b9d5c5d963ed9ba938354f7b3df453ddf9
Matt Ferraro <matt@planet.com>
2016-01-14 18:39:20 -0800

    misc updates

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Welcome to ScatDat! Your number 1 solution to your number 2 problem!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6000)
+    app.run(host='0.0.0.0', port=5000)
