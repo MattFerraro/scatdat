bb11d397bc196b4841427852bc4ef6067d18b876
Matt Ferraro <matt@planet.com>
2016-01-14 19:23:30 -0800

    Most of app.py is done

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+import flask
+import api
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Welcome to ScatDat! Your number 1 solution to your number 2 problem!'
+
+
+@app.route('/events')
+def get_events():
+    all_events = api.get_events()
+    return flask.jsonify({"events": all_events})
+
+
+@app.route('/events', methods=['POST'])
+def post_event():
+    stall_id = request.args.get('stall_id', None)
+    door_open = request.args.get('door_open', None)
+    batt_v = request.args.get('batt_v', None)
+    return flask.jsonify(api.new_event(stall_id, door_open, batt_v))
+
+
+@app.before_first_request
+def initialize():
+    api.initialize_db("scatdat.db")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
